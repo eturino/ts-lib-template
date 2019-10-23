a3ca8e9bb969a20aa67d40e766e59202b17063fb
Eduardo Turiño <eturino@plandek.com>
2019-10-23 11:01:33 +0100

    [ci skip] Updates

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"identity","url":"globals.html#identity","classes":"tsd-kind-function tsd-has-type-parameter"}]};
